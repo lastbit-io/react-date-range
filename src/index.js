9ac13a905ecc0678af8f9d823d6a14e55c37b0b8
ronnie <ronnie.bhatt.16@gmail.com>
2022-09-09 13:04:06 +0530

    update: changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import './index.css';
-
 export { default as DateRange } from './components/DateRange';
 export { default as Calendar } from './components/Calendar';
 export { default as DateRangePicker } from './components/DateRangePicker';
